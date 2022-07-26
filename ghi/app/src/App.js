14fc096078381f5c0cd7d7b4b61133bb0ae132d1
benny.huang <bhuang997@gmail.com>
2022-07-25 18:53:25 -0700

    Completed Location & Conference Form with React

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,36 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './Nav';
-import React from 'react';
+import AttendeesList from './AttendeesList'
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm'
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <React.Fragment>
+    <>
     <Nav />
     <div className="container">
-      <table className="table table-striped">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Conference</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{ attendee.name }</td>
-                <td>{ attendee.conference }</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <ConferenceForm/>
+      {/* <LocationForm/> */}
+      {/* <AttendeesList attendees={props.attendees} /> */}
     </div>
-    </React.Fragment>
+    </>
   );
 }
 
